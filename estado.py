85093e084542c4f6a8f3ff60337fe277ab3c3501
fc50025 <fc50025@alunos.fc.ul.pt>
2019-02-07 20:01:47 +0000

    ☯

---
+++ estado.py
@@ -0,0 +1,24 @@
+
+class Estado:
+    def __init__(self,size,block_x,block_y,positions=[]):
+        pass
+
+
+    def percept(self,position):
+        pass
+
+    def __lt__(self,state):
+        pass
+
+    def __eq__(self,state):
+        pass
+
+    def __hash__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def from_file(self,file_name):
+        pass
+
---
+++ game.py
@@ -0,0 +1,52 @@
+
+class Game(Problem):
+    def __init__(self,inicial,goal = None):
+        pass
+
+    
+    def actions(self,estado):
+        pass
+
+
+    def result(self,state,action):
+        """
+
+        :param state: an instance of state
+        :param action:
+        :return:
+        """
+        pass
+
+
+    def goal_test(self,state):
+        """
+
+        :param state: an instance of state
+        :return: true if is the final state
+        """
+        pass
+
+
+    def path_cost(self,c,state1,action,state2):
+        pass
+
+    def h1(self,no):
+        """
+
+        :param no: is a node
+        :return: returns the value of the state (a bigger return is a better state)
+        """
+        pass
+
+
+#############################
+
+#res = depth_first_tree_search(game)
+#res = depth_first_graph_search(game)
+#res = uniform_cost_search(game)
+#res = iterative_deepening_search(game)
+#res = depth_limited_search(game,10)
+#res = best_first_graph_search(game,game.h2)
+#res = best_first_graph_search(game,game.h3)
+#res = astar_search(game,game.h3)
+#Para correr tem descomentar o algoritmo que quer
